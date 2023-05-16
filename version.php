bbe681223f884070650590c5b3e26c984aa2c87b
evsoldatkin <e.v.soldatkin@gmail.com>
2023-05-16 09:57:26 +0300

    v fix

--- version.php
+++ version.php
@@ -1,6 +1,6 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023051200;
-$plugin->requires  = 2022071100;
+$plugin->version   = 2023051600;
+$plugin->requires  = 2020010100;
 $plugin->component = 'auth_cito1c';
